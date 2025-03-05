de3ca85349ed4eae605f4cefbe13c6a5d1afc55f
Raphael Oliveira <raphael.foliveira1@gmail.com>
2025-03-04 21:25:22 -0300

    refactor: create adapter functions for model messages

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import asyncio
-import dataclasses
-from pd_ai import models
-from pd_ai.repository import messages_repository
 from pd_ai.agent import agent
 from pydantic_ai.agent import AgentRunResult
 from pydantic_ai.messages import (
@@ -13,7 +10,7 @@ from pydantic_ai.messages import (
     ToolCallPart,
 )
 from pd_ai.repository import db
-from pd_ai.helpers import ModelMessageTypeAdapter
+from pd_ai.adapters import model_message_adapter
 
 
 def print_all_messages(response: AgentRunResult[str]):
@@ -26,21 +23,6 @@ def print_all_messages(response: AgentRunResult[str]):
                 print(f"System: {p.content}")
 
 
-async def get_messages() -> list[ModelMessage]:
-    db_messages = await messages_repository.get_all()
-    messages: list[ModelMessage] = []
-    for db_message in db_messages:
-        messages.append(ModelMessageTypeAdapter.validate_python(db_message.content))
-    return messages
-
-
-async def save_messages(messages: list[ModelMessage]):
-    for m in messages:
-        await messages_repository.create(
-            models.MessageBase(content=dataclasses.asdict(m))
-        )
-
-
 async def chat_loop():
     while True:
         user_prompt = input("User: ")
@@ -48,14 +30,14 @@ async def chat_loop():
             print("bye!")
             break
 
-        messages: list[ModelMessage] = await get_messages()
+        messages: list[ModelMessage] = await model_message_adapter.get_messages()
         response = await agent.run(
             user_prompt=user_prompt,
             message_history=messages,
             deps={},
         )
         new_messages: list[ModelMessage] = response.new_messages()
-        await save_messages(new_messages)
+        await model_message_adapter.save_messages(new_messages)
         print(f"Bot: {response.data}")
 
 
